0dffa56f279c26878bb4333d8e11093848b66e9b
Clément P. (Cleymax) <clemperrin1@gmail.com>
2020-02-26 02:17:20 +0100

    :tada: Add options parser

---
+++ main.py
@@ -0,0 +1,43 @@
+from argparse import ArgumentParser
+from optparse import OptionParser, OptionGroup
+
+
+def main():
+    usage = 'python main.py [options] arg1 arg2 arg...'
+
+    parser = ArgumentParser(
+        usage=usage,
+        description='WorldScanner: The best scanner in the minecraft world',
+        epilog='Here is the help for all possible arguments and options.',
+        prog='WorldScanner'
+    )
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0.0')
+
+    parser.add_argument('--region', action='store_true', help='search in region file')
+    parser.add_argument('--player', action='store_true', help='search in player data file', default=True)
+    parser.add_argument('--container', action='store_true', help='search in container in world')
+    parser.add_argument('--tile-entity', action='store_true', help='search in title entities in world')
+    parser.add_argument('--inventory', action='store_true', help='search in player''s inventory')
+    parser.add_argument('--enderchest', action='store_true', help='search in player''s enderchest')
+
+    parser.add_argument('-x', dest='chunk', help='x coordinate of a chunk')
+    parser.add_argument('-z', dest='chunk', help='z coordinate of a chunk')
+
+    parser.add_argument('--containers',
+                        choices=['chest', 'trapped_chest', 'dropper', 'dispenser', 'hopper', 'armorstand', 'item',
+                                 'furnace_minecart', 'chest_minecart', 'hopper_minecart', 'item_frame', '*'],
+                        default='*', help='type of the container to search')
+    parser.add_argument("--type", choices=['item', 'block', 'entity', 'enchantments', 'count', 'player'], type=str,
+                        help='the type of think to search in region, player data or somewhere else')
+    parser.add_argument('--count', type=int, default=1, help='minimum amount of item to get')
+
+    needed_group = parser.add_argument_group('needed arguments')
+    needed_group.add_argument('-w', '--world', type=str, help='path of the world''s folder')
+    needed_group.add_argument('-id', '--id', action='extend', type=str, nargs="+",
+                              help='list of item to search in the world')
+
+    args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
