f7221f9d0e94c212b3d7731f690bb8b6ac73a1d4
Clément P. (Cleymax) <clemperrin1@gmail.com>
2020-02-26 23:29:14 +0100

    :rocket: Edit main

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ def main():
 
     parser.add_argument('--region', action='store_true', help='search in region file')
     parser.add_argument('--player', action='store_true', help='search in player data file', default=True)
-    parser.add_argument('--container', action='store_true', help='search in container in world')
     parser.add_argument('--tile-entity', action='store_true', help='search in title entities in world')
     parser.add_argument('--inventory', action='store_true', help='search in player''s inventory', default=True)
     parser.add_argument('--enderchest', action='store_true', help='search in player''s enderchest', default=True)
@@ -29,7 +28,7 @@ def main():
                                  'furnace_minecart', 'chest_minecart', 'hopper_minecart', 'item_frame', '*'],
                         default='*', help='type of the container to search')
     parser.add_argument("--type", choices=['item', 'block', 'entity', 'enchantments', 'count', 'player'], type=str,
-                        help='the type of think to search in region, player data or somewhere else')
+                        default='item', help='the type of think to search in region, player data or somewhere else')
     parser.add_argument('--count', type=int, default=1, help='minimum amount of item to get')
 
     needed_group = parser.add_argument_group('needed arguments')
@@ -45,8 +44,9 @@ def main():
     if not os.path.exists(args.world):
         parser.error('World not found !')
 
-    player = Player(args)
-    player.scan()
+    if args.player:
+        player = Player(args)
+        player.scan()
 
 
 if __name__ == '__main__':
